589932ada2987f4adc703c8f594ba62e09e57dec
kutushova95 <aruukekutushodev@gmail.com>
2023-03-28 16:37:43 +0600

    clesscsmp

---
+++ script.js
@@ -0,0 +1,15 @@
+let swiper = new Swiper(".mySwiper", {
+    spaceBetween: 50,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+let swiper2 = new Swiper(".mySwiper2", {
+    direction: "vertical",
+    spaceBetween: 50,
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    },
+});
